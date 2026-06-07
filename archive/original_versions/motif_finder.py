e42808ae4b872eea318dcdd88ac6213997e84c24
Hazrat Maghaz <maaz28608@gmail.com>
2026-06-07 11:37:24 +0500

    Archive original bioinformatics scripts

---
+++ archive/original_versions/motif_finder.py
@@ -0,0 +1,18 @@
+print("Welcome to the Motif Finder!")
+
+motif = ('ATCG', 'ATC','AGC')
+
+sequence_1 = input("Enter the DNA sequence you want to check motif's: ").upper()
+  
+          
+for m in sequence_1:
+    if m in sequence_1:
+        print("Motifs is present there")
+        break
+    else:
+        print("No Motifs founds")
+
+
+# print("The Process is been added there")        
+
+# print("The process is get completed sucessfully    ")
