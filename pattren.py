54bb017f123d42ad483ddaa4f95a40549c5c3857
Hazrat Maghaz <maaz28608@gmail.com>
2026-06-07 11:41:39 +0500

    Remove duplicate root-level practice scripts

--- pattren.py
+++
@@ -1,9 +0,0 @@
-# Print a specific pattren through python
-user = int(input("input rows: "))
-
-
-for i in range(1,user):
-    # print("%")
-    for j in range(i,user + 1):
-        print("*" , end= " ")
-    print()    
